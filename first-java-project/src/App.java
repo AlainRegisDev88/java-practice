425978c80d95cec22019910864254aec2f4039f7
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-11 21:22:54 +0200

    initial commit

---
+++ first-java-project/src/App.java
@@ -0,0 +1,10 @@
+public class App{
+    public static void main(String [] args) throws Exception {
+        System.out.println("Hello World");
+        int x = 5;
+        int y = 7;
+
+        int sum = x + y;
+        System.out.println(sum);
+    }
+}
