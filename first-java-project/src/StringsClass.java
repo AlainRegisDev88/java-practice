222d2762e19da81ef9cc6734ee8e45477c89683f
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-15 10:31:10 +0200

    add the stringbuilder and get its type

--- first-java-project/src/StringsClass.java
+++ first-java-project/src/StringsClass.java
@@ -8,7 +8,15 @@ public class StringsClass {
         System.out.println(text.trim().indexOf("esi"));
         System.out.println(text.trim().replace("Ashesi", "My"));
         System.out.println(text.trim().split(" "));
+        
+        // string builder: to avoid creation of multiple strings(as it is immutable)
+        StringBuilder inc = new StringBuilder();
+        for (int i = 0; i < 5; i++){
+            inc.append(i).append(",");
+        }
+        String incString = inc.toString();
+
+        // getting typeof() => python:  java version
+        System.out.println(incString.getClass().getSimpleName());
     }
-
-
 }
