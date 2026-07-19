385744d509ee5e16bcbdea3a7bf5a9e9d439a5ae
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-19 11:57:17 +0200

    add triangle

--- first-java-project/src/ConditionalStatements.java
+++ first-java-project/src/ConditionalStatements.java
@@ -89,6 +89,15 @@ class Grids{
         }
     }
 
+    public static void triangle(){
+        for(int i = 0; i <= 4; i++){
+            for (int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
 
 }
 
