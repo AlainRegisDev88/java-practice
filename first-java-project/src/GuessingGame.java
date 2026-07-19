a12a13e5c9dff06f5436e3eccad9476facb373d4
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-19 12:19:44 +0200

    create a randomize method

--- first-java-project/src/GuessingGame.java
+++ first-java-project/src/GuessingGame.java
@@ -1,5 +1,14 @@
+import java.util.Random;
 
+class RandomNumber{
+    private int randomize(){
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(101);
+    }
+}
 
 public class GuessingGame {
+
+    
 
 }
