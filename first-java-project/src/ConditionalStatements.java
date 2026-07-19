1d175c73633f35849452fe356a494407909a9bf4
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-19 11:57:32 +0200

    add reverse triangle

--- first-java-project/src/ConditionalStatements.java
+++ first-java-project/src/ConditionalStatements.java
@@ -98,7 +98,18 @@ class Grids{
         }
     }
 
-
+    public static void reverseTriangle(){
+        for(int i = 0; i <= 4; i++){
+            
+            for (int k = 3; k>=i; k--){
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
 
 public class ConditionalStatements {
