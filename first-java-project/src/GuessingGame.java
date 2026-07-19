555afb169ef10eb92a2f9e9d91d0489306856c38
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-07-19 12:28:14 +0200

    add the input function and instructions

--- first-java-project/src/GuessingGame.java
+++ first-java-project/src/GuessingGame.java
@@ -1,14 +1,33 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class RandomNumber{
+
     private int randomize(){
         Random rand = new Random();
         int randomNumber = rand.nextInt(101);
+        return randomNumber;
+    }
+     private int getNumber(){
+        Scanner in = new Scanner(System.in);
+        int number = Integer.parseInt(in.nextLine());
+        return number;
+     }
+
+    public static void randomNumberGame(){
+        System.out.println("Welcome to our guessing game:");
+        System.out.println("I will set a number and you will guess until you get it!!:");
+        System.out.println("I will tell you if the number is higher or lower than your guess");
+        System.out.println("Aim for lower guesses");
+        System.out.println("");
+        System.out.print("Let's start, enter your guess: ");
     }
 }
 
 public class GuessingGame {
 
-    
+    public static void main(String[] args) {
+        RandomNumber.randomNumberGame();
+    }
 
 }
