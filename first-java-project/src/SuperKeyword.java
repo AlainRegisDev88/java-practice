6b1cb8970d867bbbd27cbed15ae989cbce240494
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-08-10 11:15:15 +0200

    super and this keyword

---
+++ first-java-project/src/SuperKeyword.java
@@ -0,0 +1,31 @@
+
+
+class GrandParent {
+    GrandParent(){
+        System.out.println("I am 60 years old!");
+    }
+}
+
+class Parent extends GrandParent{
+    Parent(){
+        System.out.println("I am 40 years old!");
+    }
+}
+
+class Child extends Parent {
+    Child() {
+        super();
+        System.out.println("I am 20 years old!");
+    }
+
+    Child(String status) {
+        this();
+        System.out.println("I am also " + status);
+    }
+}
+
+public class SuperKeyword {
+    public static void main(String[] args) {
+        Child obj = new Child("The favorite");
+    }
+}
