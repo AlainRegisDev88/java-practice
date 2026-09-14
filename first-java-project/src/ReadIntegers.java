4e159383fd296705035c616ad7599d3c6b490069
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-09-14 23:16:24 +0000

    add the do while loop logic

--- first-java-project/src/ReadIntegers.java
+++ first-java-project/src/ReadIntegers.java
@@ -4,26 +4,40 @@ public class ReadIntegers {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Boolean valid = false;
+        // using a while loop
+        // Boolean valid = false;
 
-        System.out.print("Please enter a number between 1 and 10 inclusive...: ");
+        // System.out.print("Please enter a number between 1 and 10 inclusive...: ");
 
-        while(!valid){
-            int inputValue = Integer.parseInt(input.nextLine());
-            if(inputValue < 0){
-                System.out.println("The number "+ inputValue + " you entered is not between 0 and 10: Please try again!!!");
-            }else if(inputValue > 10){
-                System.out.println("The number should be less than 10: Try again!!!");
-            }else{
-                System.out.println("The number you entered is " + inputValue + " Thank you!!!");
-                valid = true;
-            }
-        }
+        // while(!valid){
+        // int inputValue = Integer.parseInt(input.nextLine());
+        // if(inputValue < 0){
+        // System.out.println("The number "+ inputValue + " you entered is not between 0
+        // and 10: Please try again!!!");
+        // }else if(inputValue > 10){
+        // System.out.println("The number should be less than 10: Try again!!!");
+        // }else{
+        // System.out.println("The number you entered is " + inputValue + " Thank
+        // you!!!");
+        // valid = true;
+        // }
+        // }
+
+        // using a do while loop
+
+        int n;
+        do {
+            System.out.println("Enter a number between 1 and 10: ");
+            n = input.nextInt();
+        } while (n < 1 || n > 10);
+
+        System.out.println("The number "+ n + " is between 1 and 10");
+
         input.close();
         // do{
-        //     int inputValue = Integer.parseInt(input.nextLine())
+        // int inputValue = Integer.parseInt(input.nextLine())
 
-        // }while (inputValue < 0 && inputValue > 10); 
+        // }while (inputValue < 0 && inputValue > 10);
     }
 
 }
